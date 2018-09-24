1934d6d0f664f72a7b2cd9b9027e4d88fad5f85a
kuehma02 <37346015+kuehma02@users.noreply.github.com>
2018-09-24 11:56:10 -0500

    Update model.js

--- exercise2/model.js
+++ exercise2/model.js
@@ -1,1 +1,50 @@
+class Item {
+    constructor(name,quantity,price,store,section,priority){
+        this._name = name;
+        this._quantity = quantity;
+        this._price = price;
+        this._store = store;
+        this._section = section;
+        this._priority = priority;
+        this._purchased = false;
+    }
 
+    get purchased() {
+        return this._purchased
+    }
+
+    set purchased(newVal) {
+        if (newVal != true || newVal != false){
+            console.error("Must be a boolean")
+        }
+        else {
+            this._purchased = newVal;
+        }
+    }
+}
+
+class ShoppingList {
+    constructor(){
+        this._items = [];
+    }
+
+    addItem(item){
+        this._items.push(item);
+    }
+    
+    cleanList(){
+        let len = this._items.length
+        count = 0
+        for (item in this._items){
+            if (item._purchased == true){
+                this._items.splice(count,1)
+            }
+            count += 1;
+        }
+    }
+
+    emptyList() {
+        this.items = []
+    }
+
+}
