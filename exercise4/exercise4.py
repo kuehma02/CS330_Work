651cff96e591ff0d66df6cfb540bdd7d0326a7c7
kuehma02 <kuehma02@luther.edu>
2018-10-28 23:20:10 -0500

    finished 4

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -26,33 +26,32 @@ def get_n_primes(n: int) -> list:
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
+    if 'number' in request.args:
+        num = request.args['number']
+        return make_response(redirect(url_for('get_primes', n=num)))
+    else:
         numPrimes = request.cookies.get('number')
         if numPrimes:
             return make_response(redirect(url_for('get_primes', n=numPrimes)))
         else:
             return make_response(redirect(url_for('ask_a_number'))) 
 
-@app.route('/<int:n>', methods=['GET', 'POST'])
-def get_primes(n):
-    if request.method == 'GET':
-        primeList = get_n_primes(n)
-        response = make_response(render_template('prime_table.html', primeList = primeList))
-        response.set_cookie('number', str(n))
-    return response
-    
 
-    
+@app.route('/<int:n>', methods=['GET'])
+def get_primes(n):
+    primeList = get_n_primes(n)
+    response = make_response(render_template('prime_table.html', primeList = primeList))
+    response.set_cookie('number', str(n))
+    return response
 
 @app.route('/ask', methods=['GET', 'POST'])
 def ask_a_number():
     if request.method == 'GET':
         return render_template('ask.html')
-    if request.form.get('click'):
+    else:
         enteredNum = request.form['numPrimes']
         if enteredNum != "":
-            response = make_response(redirect(url_for('get_primes', n=enteredNum)))
-            return response
+            return redirect(url_for('index', number = enteredNum))
         else:
             return render_template('ask.html')   
 
