f235b537175434837def7ffa9656b5b743a6ed6a
kuehma02 <kuehma02@luther.edu>
2018-11-23 17:31:11 -0600

    finished exercise 6

---
+++ exercise6/exercise6.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import url_for
+from flask import redirect
+from flask import make_response
+from flask import session, escape
+import os
+
+app = Flask(__name__)
+app.secret_key = b'^\x97\x91S!\xc1\x1b\xce\x0b\x92\xd8\x15Q$\xfe\x9f'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
